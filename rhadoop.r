e7c082a281d782731b82ca91ec9a3c9308966135
Rainbow <33627940+TianSong1991@users.noreply.github.com>
2017-11-17 22:22:27 +0800

    Add files via upload

---
+++ rhadoop.r
@@ -0,0 +1,7 @@
+Sys.setenv(HADOOP_CMD="/home/rainbow/下载/hadoop-2.7.0/bin/hadoop")
+Sys.setenv(HADOOP_STREAMING="/home/rainbow/下载/hadoop-2.7.0/share/hadoop/tools/lib/hadoop-streaming-2.7.0.jar")
+
+library(rJava)
+library(rhdfs)
+library(rmr2)
+
